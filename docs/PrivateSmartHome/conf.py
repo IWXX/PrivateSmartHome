93f200bf982b761df30bc05a6f30f6ff1926b387
Mr.Y <1059392229@qq.com>
2017-12-16 19:54:48 +0800

    test2

--- docs/PrivateSmartHome/conf.py
+++ docs/PrivateSmartHome/conf.py
@@ -48,8 +48,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PrivateSmartHome'
-copyright = u'2017, test1'
-author = u'test1'
+copyright = u'2017, Yuancong'
+author = u'Yuancong'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
